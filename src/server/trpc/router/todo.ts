ac292d1be146abbb1b32431a812592d7e2a9dd66
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2022-12-21 16:50:51 -0500

    Update todo.ts

--- src/server/trpc/router/todo.ts
+++ src/server/trpc/router/todo.ts
@@ -1,45 +1,52 @@
 import { z } from "zod";
-
 import { router, protectedProcedure } from "../trpc";
+
+const TaskStatus = z.enum(["INPROGRESS", "DONE", "CANCELED", "ABANDONED"]);
 
 export const todoRouter = router({
   create: protectedProcedure
-    .input(z.object({ title: z.string() }))
-    .mutation(async ({ctx, input}) => {
+    .input(z.object({ title: z.string().min(2) }))
+    .mutation(async ({ ctx, input }) => {
       return await ctx.prisma.task.create({
         data: {
-          title: ctx.session.user.id,
-          userId: input.title
-        }
-      })
-
+          title: input.title,
+          userId: ctx.session.user.id,
+        },
+      });
     }),
   all: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.task.findMany({
-      where: { 
-        userId: ctx.session.user.id 
-      }
+      where: {
+        userId: ctx.session.user.id,
+      },
     });
   }),
- update: protectedProcedure
- .input(z.object({id: z.number().int(), title: z.string()}))
- .mutation(async ({ctx, input}) => {
-    return await ctx.prisma.task.update({
-      where: {
-        id: input.id
-      },
-      data: {
-        title: input.title
-      }
-    })
- }),
- delete: protectedProcedure
- .input(z.object({id: z.number().int(), title: z.string()}))
- .mutation(async ({ctx, input}) => {
-    return await ctx.prisma.task.delete({
-      where: {
-        id: input.id,
-      }
-    })
- })
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number().int(),
+        title: z.string().min(2),
+        status: TaskStatus,
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.task.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+          status: input.status,
+        },
+      });
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.number().int() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.task.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
 });
